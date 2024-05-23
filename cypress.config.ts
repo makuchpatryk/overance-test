185758f1752b97da7dd335c1c6c540bbe9ce622a
Pat <makuchpatryk@gmail.com>
2024-05-23 23:39:50 +0200

    first commit

--- cypress.config.ts
+++ cypress.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   },
 
   e2e: {
-    baseUrl: "http://localhost:5173/overance-test",
+    baseUrl: "http://localhost:5173",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
