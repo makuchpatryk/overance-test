1efa4e217ae5f488895322ed47cace0a824098e4
Pat <makuchpatryk@gmail.com>
2024-05-23 23:41:42 +0200

    first commit

--- cypress.config.ts
+++ cypress.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   },
 
   e2e: {
-    baseUrl: "http://localhost:5173",
+    baseUrl: process.env.BASE_URL,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
